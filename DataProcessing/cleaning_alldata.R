7743a60755a808c1628074d2da330ccdf0bfe703
Geoffrey Broadhead <gtb49@cornell.edu>
2021-03-31 11:50:39 -0400

    Updating 2017 and 2018 data processing .R and .Rmd and beginning to clean the complete dataset

---
+++ DataProcessing/cleaning_alldata.R
@@ -0,0 +1,8 @@
+#2017 and 2018 datasheets have been combined - still lots of irrlevant entries and issues with eclosion date numbers, date format etc. 
+#also need to forward fill a lot of things before filtering for complete data
+#Getting down to actual experimental data here (hopefully)
+
+library(readxl)
+alldata<-read_excel("E:/Hahn Lab/CircadianRhag/DataProcessing/alldata.xlsx")
+
+
